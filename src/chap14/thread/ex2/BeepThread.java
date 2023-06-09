bc35b9aaef30e6f7cbe16eb2d3f35f8c1d0a9236
cheeeeze <nyj9349@hanmail.net>
2023-06-09 16:33:45 +0900

    thread 기초

---
+++ src/chap14/thread/ex2/BeepThread.java
@@ -0,0 +1,24 @@
+package chap14.thread.ex2;
+
+import java.awt.*;
+
+public class BeepThread extends Thread {
+	
+	@Override
+	public void run() {
+		
+		for( int i = 0; i < 5; i++ ) {
+			
+			Toolkit toolkit = Toolkit.getDefaultToolkit();
+			toolkit.beep();
+			
+			try {
+				Thread.sleep( 1000 );
+			}
+			catch( InterruptedException e ) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+}
